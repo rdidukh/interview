d363e16071df789c6e9f50b7389dbb6efa65b90e
r.didukh <r.didukh@samsung.com>
2013-04-03 16:03:33 +0300

    Improved 'Write a code to reverse a C-style string.': added one more solution.

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -6,22 +6,46 @@
 #include <cstring>
 #include <iostream>
 
-int main(int argc, char* argv[])
+void reverse_1(const char *str, char *buffer)
 {
-    int len = strlen(argv[1]);
-
-    char *buf = new char[len];
+    int len = strlen(str);
     
     int i;
     for(i = 0; i <= len/2; i++)
     {
-        buf[i] = argv[1][len-i-1];
-        buf[len-i-1] = argv[1][i];
+        buffer[i] = str[len-i-1];
+        buffer[len-i-1] = str[i];
     }
+    buffer[len] = 0;
+}
 
-    std::cout << buf << std::endl;
+void reverse_2(char * str)
+{
+    if(*str == 0) return;
 
-    delete[] buf;
+    char *end = str;
+    while(*end) end++;
+    end--;
+
+    while(str < end)
+    {
+        char tmp = *str;
+        *str++ = *end;
+        *end-- = tmp;        
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    char buffer[512];
+
+    reverse_1(argv[1], buffer);
+    
+    std::cout << "reverse_1: " << buffer << std::endl; 
+    
+    reverse_2(buffer);
+
+    std::cout << "reverse_2: " << buffer << std::endl;
 
     return 0;
 }
